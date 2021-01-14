e08b5f72b75c9bac9a9bb05e8b567d974a08a969
Shriya <>
2021-01-13 21:15:10 -0500

    length and merge done

--- Radix.java
+++ Radix.java
@@ -5,4 +5,19 @@ public class Radix{
     double ans = (double)n % Math.pow(10, col+1);
     return ((int)(ans / Math.pow(10 ,col)));
   }
+public static int length(int n){
+  n = Math.abs(n);
+  if (n == 0){
+    return 1;
+  }
+  return ((int)Math.log10(n)+1);
+}
+public static void merge(SortableLinkedList original, SortableLinkedList[]buckets){
+  for (SortableLinkedList i : buckets){
+            if (i.size() > 0){
+                original.extend(i);
+            }
+        }
+}
+
 }
